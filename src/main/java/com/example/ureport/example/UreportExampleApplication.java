e0545f9187d23223d2d8480320fc48aba547b9b3
mxvc <410518072@qq.com>
2025-12-02 21:38:50 +0800

    init

---
+++ src/main/java/com/example/ureport/example/UreportExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ureport.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UreportExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UreportExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ureport/example/UreportExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ureport.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UreportExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
